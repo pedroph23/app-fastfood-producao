5b663bed6442799af1682e842bf0a8b6da95d0ba
Pedro Ramalho <pedro.ramalho@sysmap.com.br>
2024-01-06 15:49:13 -0300

    Ajuste

--- app/src/test/java/br/com/appfastfood/AppFastfoodApplicationTests.java
+++ app/src/test/java/br/com/appfastfood/AppFastfoodApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.appfastfood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AppFastfoodApplicationTests {
 
 	@Test
